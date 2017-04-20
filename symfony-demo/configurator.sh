7858dabe1db2f061f4ffa6fb391118884453fe22
Jan Hryniuk <jasiekhryniuk@gmail.com>
2017-04-20 10:41:42 +0200

    Improved bash script

--- symfony-demo/configurator.sh
+++ symfony-demo/configurator.sh
@@ -1,47 +1,86 @@
 #!/usr/bin/env bash
 
-DEMO_PATH="symfony-demo"
-SKELETON_PATH="php-docker-skeleton"
-SKELETON_FILES=(
-"docker"
-".project_name"
-"docker.sh"
-"docker-compose.local.yml"
-"docker-compose.yml"
-)
+set -o errexit
+set -o pipefail
+set -o nounset
+
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+DEMO_PATH="${__dir}/symfony-demo"
 DEMO_VERSION="0.0.1"
 
-function download-symfony-demo
+SKELETON_PATH="${__dir}/php-docker-skeleton"
+
+SKELETON_FILES=(
+    docker
+    .project_name
+    docker.sh
+    docker-compose.local.yml
+    docker-compose.yml
+)
+
+function downloadSymfonyDemo
 {
-    git clone git@github.com:symfony/symfony-demo.git "${DEMO_PATH}"
+    if [ ! -d "${DEMO_PATH}" ]; then
+        echo -e "\033[1;36mSymfony-demo downloading..\033[0m"
+        git clone git@github.com:symfony/symfony-demo.git "${DEMO_PATH}"
+    else
+        echo -e "\033[1;36mSymfony-demo\033[0m already donwloaded"
+    fi
 }
 
-function download-docker-skeleton
+function downloadDockerSkeleton
 {
-    git clone git@github.com:PGSSoft/php-docker-skeleton.git "${SKELETON_PATH}"
+    if [ ! -d "${SKELETON_PATH}" ]; then
+        echo -e "\033[1;36mDocker-skeleton downloading..\033[0m"
+        git clone git@github.com:PGSSoft/php-docker-skeleton.git "${SKELETON_PATH}"
+    else
+        echo -e "\033[1;36mDocker-skeleton\033[0m already downloaded"
+    fi
 }
 
-function copy-files-from-skeleton
+function copyFilesFromSkeleton
 {
     for i in "${SKELETON_FILES[@]}"
     do
-        cp -r "${SKELETON_PATH}"/"${i}" "${DEMO_PATH}"/"${i}"
+        if [ ! -f "${DEMO_PATH}/${i}" ] | [ ! -d "${DEMO_PATH}/${i}" ]; then
+            echo -e "Copy \033[1;36m${SKELETON_PATH}/${i}\033[0m to \033[1;36m${DEMO_PATH}/${i}\033[0m .."
+            cp -r "${SKELETON_PATH}/${i}" "${DEMO_PATH}/${i}"
+        else
+            echo -e "\033[1;36m${DEMO_PATH}/${i}\033[0m already exists."
+        fi
     done
 }
 
-function configure-docroot
+function configureDocroot
 {
-    sed -i "s/PROJECT_INDEX_FILE:=\"index.php\"/PROJECT_INDEX_FILE:=\"app.php\"/g" "${DEMO_PATH}"/docker.sh
-    sed -i "s/PROJECT_DEV_INDEX_FILE:=\"index_dev.php\"/PROJECT_DEV_INDEX_FILE:=\"app_dev.php\"/g" "${DEMO_PATH}"/docker.sh
+    if grep -q "PROJECT_INDEX_FILE:=\"app.php\"" "${DEMO_PATH}/docker.sh"; then
+        echo -e "\033[1;36mPROJECT_INDEX_FILE:=\"app.php\"\033[0m already set in \033[1;36m${DEMO_PATH}/docker.sh\033[0m"
+    else
+        echo -e "\033[1;36mPROJECT_INDEX_FILE:=\"app.php\"\033[0m set in \033[1;36m${DEMO_PATH}/docker.sh\033[0m"
+        sed -i "s/PROJECT_INDEX_FILE:=\"index.php\"/PROJECT_INDEX_FILE:=\"app.php\"/g" "${DEMO_PATH}/docker.sh"
+    fi
+
+    if grep -q "PROJECT_DEV_INDEX_FILE:=\"app_dev.php\"" "${DEMO_PATH}/docker.sh"; then
+        echo -e "\033[1;36mPROJECT_DEV_INDEX_FILE:=\"app_dev.php\"\033[0m already set in \033[1;36m${DEMO_PATH}/docker.sh\033[0m"
+    else
+        echo -e "\033[1;36mPROJECT_DEV_INDEX_FILE:=\"app_dev.php\"\033[0m set in \033[1;36m${DEMO_PATH}/docker.sh\033[0m"
+        sed -i "s/PROJECT_DEV_INDEX_FILE:=\"index_dev.php\"/PROJECT_DEV_INDEX_FILE:=\"app_dev.php\"/g" "${DEMO_PATH}/docker.sh"
+    fi
 }
 
-function configure-version
+function configureVersion
 {
-    sed -i "/\"name\": \"symfony\/symfony-demo\",/a \"version\": \"${DEMO_VERSION}\"," "${DEMO_PATH}"/composer.json
+    if grep -q "\"version\": \"${DEMO_VERSION}\"," "${DEMO_PATH}/composer.json"; then
+        echo -e "\033[1;36m\"version\": \"${DEMO_VERSION}\",\033[0m already set in \033[1;36m${DEMO_PATH}/composer.json\033[0m"
+    else
+        echo -e "\033[1;36m\"version\": \"${DEMO_VERSION}\",\033[0m set in \033[1;36m${DEMO_PATH}/composer.json\033[0m"
+        sed -i "/\"name\": \"symfony\/symfony-demo\",/a \"version\": \"${DEMO_VERSION}\"," "${DEMO_PATH}/composer.json"
+    fi
 }
 
-download-symfony-demo
-download-docker-skeleton
-copy-files-from-skeleton
-configure-docroot
-configure-version
+downloadSymfonyDemo
+downloadDockerSkeleton
+copyFilesFromSkeleton
+configureDocroot
+configureVersion
