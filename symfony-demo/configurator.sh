03169091e61bca8d7f04972504dfc849e77b2b21
Jan Hryniuk <jasiekhryniuk@gmail.com>
2017-04-20 15:27:23 +0200

    Improved configurator for symfony

--- symfony-demo/configurator.sh
+++ symfony-demo/configurator.sh
@@ -19,23 +19,26 @@ SKELETON_FILES=(
     docker-compose.yml
 )
 
+COLOR_START="\033[1;36m"
+COLOR_STOP="\033[0m"
+
 function downloadSymfonyDemo
 {
     if [ ! -d "${DEMO_PATH}" ]; then
-        echo -e "\033[1;36mSymfony-demo downloading..\033[0m"
+        echo -e "${COLOR_START}Symfony-demo downloading..${COLOR_STOP}"
         git clone git@github.com:symfony/symfony-demo.git "${DEMO_PATH}"
     else
-        echo -e "\033[1;36mSymfony-demo\033[0m already donwloaded"
+        echo -e "${COLOR_START}Symfony-demo${COLOR_STOP} already donwloaded"
     fi
 }
 
 function downloadDockerSkeleton
 {
     if [ ! -d "${SKELETON_PATH}" ]; then
-        echo -e "\033[1;36mDocker-skeleton downloading..\033[0m"
+        echo -e "${COLOR_START}Docker-skeleton downloading..${COLOR_STOP}"
         git clone git@github.com:PGSSoft/php-docker-skeleton.git "${SKELETON_PATH}"
     else
-        echo -e "\033[1;36mDocker-skeleton\033[0m already downloaded"
+        echo -e "${COLOR_START}Docker-skeleton${COLOR_STOP} already downloaded"
     fi
 }
 
@@ -44,10 +47,10 @@ function copyFilesFromSkeleton
     for i in "${SKELETON_FILES[@]}"
     do
         if [ ! -f "${DEMO_PATH}/${i}" ] | [ ! -d "${DEMO_PATH}/${i}" ]; then
-            echo -e "Copy \033[1;36m${SKELETON_PATH}/${i}\033[0m to \033[1;36m${DEMO_PATH}/${i}\033[0m .."
+            echo -e "Copy ${COLOR_START}${SKELETON_PATH}/${i}${COLOR_STOP} to ${COLOR_START}${DEMO_PATH}/${i}${COLOR_STOP} .."
             cp -r "${SKELETON_PATH}/${i}" "${DEMO_PATH}/${i}"
         else
-            echo -e "\033[1;36m${DEMO_PATH}/${i}\033[0m already exists."
+            echo -e "${COLOR_START}${DEMO_PATH}/${i}${COLOR_STOP} already exists."
         fi
     done
 }
@@ -55,16 +58,16 @@ function copyFilesFromSkeleton
 function configureDocroot
 {
     if grep -q "PROJECT_INDEX_FILE:=\"app.php\"" "${DEMO_PATH}/docker.sh"; then
-        echo -e "\033[1;36mPROJECT_INDEX_FILE:=\"app.php\"\033[0m already set in \033[1;36m${DEMO_PATH}/docker.sh\033[0m"
+        echo -e "${COLOR_START}PROJECT_INDEX_FILE:=\"app.php\"${COLOR_STOP} already set in ${COLOR_START}${DEMO_PATH}/docker.sh${COLOR_STOP}"
     else
-        echo -e "\033[1;36mPROJECT_INDEX_FILE:=\"app.php\"\033[0m set in \033[1;36m${DEMO_PATH}/docker.sh\033[0m"
+        echo -e "${COLOR_START}PROJECT_INDEX_FILE:=\"app.php\"${COLOR_STOP} set in ${COLOR_START}${DEMO_PATH}/docker.sh${COLOR_STOP}"
         sed -i "s/PROJECT_INDEX_FILE:=\"index.php\"/PROJECT_INDEX_FILE:=\"app.php\"/g" "${DEMO_PATH}/docker.sh"
     fi
 
     if grep -q "PROJECT_DEV_INDEX_FILE:=\"app_dev.php\"" "${DEMO_PATH}/docker.sh"; then
-        echo -e "\033[1;36mPROJECT_DEV_INDEX_FILE:=\"app_dev.php\"\033[0m already set in \033[1;36m${DEMO_PATH}/docker.sh\033[0m"
+        echo -e "${COLOR_START}PROJECT_DEV_INDEX_FILE:=\"app_dev.php\"${COLOR_STOP} already set in ${COLOR_START}${DEMO_PATH}/docker.sh${COLOR_STOP}"
     else
-        echo -e "\033[1;36mPROJECT_DEV_INDEX_FILE:=\"app_dev.php\"\033[0m set in \033[1;36m${DEMO_PATH}/docker.sh\033[0m"
+        echo -e "${COLOR_START}PROJECT_DEV_INDEX_FILE:=\"app_dev.php\"${COLOR_STOP} set in ${COLOR_START}${DEMO_PATH}/docker.sh${COLOR_STOP}"
         sed -i "s/PROJECT_DEV_INDEX_FILE:=\"index_dev.php\"/PROJECT_DEV_INDEX_FILE:=\"app_dev.php\"/g" "${DEMO_PATH}/docker.sh"
     fi
 }
@@ -72,10 +75,20 @@ function configureDocroot
 function configureVersion
 {
     if grep -q "\"version\": \"${DEMO_VERSION}\"," "${DEMO_PATH}/composer.json"; then
-        echo -e "\033[1;36m\"version\": \"${DEMO_VERSION}\",\033[0m already set in \033[1;36m${DEMO_PATH}/composer.json\033[0m"
+        echo -e "${COLOR_START}\"version\": \"${DEMO_VERSION}\",${COLOR_STOP} already set in ${COLOR_START}${DEMO_PATH}/composer.json${COLOR_STOP}"
     else
-        echo -e "\033[1;36m\"version\": \"${DEMO_VERSION}\",\033[0m set in \033[1;36m${DEMO_PATH}/composer.json\033[0m"
+        echo -e "${COLOR_START}\"version\": \"${DEMO_VERSION}\",${COLOR_STOP} set in ${COLOR_START}${DEMO_PATH}/composer.json${COLOR_STOP}"
         sed -i "/\"name\": \"symfony\/symfony-demo\",/a \"version\": \"${DEMO_VERSION}\"," "${DEMO_PATH}/composer.json"
+    fi
+}
+
+function copyBuildXml
+{
+    if [ ! -f "${DEMO_PATH}/build.xml" ]; then
+        echo -e "Copy ${COLOR_START}${__dir}/build.xml${COLOR_STOP} to ${COLOR_START}${DEMO_PATH}/build.xml${COLOR_STOP} .."
+        cp -r "${__dir}/build.xml" "${DEMO_PATH}/build.xml"
+    else
+        echo -e "${COLOR_START}${DEMO_PATH}/build.xml${COLOR_STOP} already exists."
     fi
 }
 
@@ -84,3 +97,4 @@ downloadDockerSkeleton
 copyFilesFromSkeleton
 configureDocroot
 configureVersion
+copyBuildXml
