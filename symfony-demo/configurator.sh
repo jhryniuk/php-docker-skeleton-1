2f00e1d90eddbc6426615f9a961faaf2e6129bd0
jhryniuk <jasiekhryniuk@gmail.com>
2017-04-21 00:56:05 +0200

    Update configurator.sh

--- symfony-demo/configurator.sh
+++ symfony-demo/configurator.sh
@@ -26,7 +26,7 @@ function downloadSymfonyDemo
 {
     if [ ! -d "${DEMO_PATH}" ]; then
         echo -e "${COLOR_START}Symfony-demo downloading..${COLOR_STOP}"
-        git clone git@github.com:symfony/symfony-demo.git "${DEMO_PATH}"
+        git clone https://github.com:symfony/symfony-demo.git "${DEMO_PATH}"
     else
         echo -e "${COLOR_START}Symfony-demo${COLOR_STOP} already donwloaded"
     fi
@@ -36,7 +36,7 @@ function downloadDockerSkeleton
 {
     if [ ! -d "${SKELETON_PATH}" ]; then
         echo -e "${COLOR_START}Docker-skeleton downloading..${COLOR_STOP}"
-        git clone git@github.com:PGSSoft/php-docker-skeleton.git "${SKELETON_PATH}"
+        git clone https://github.com:PGSSoft/php-docker-skeleton.git "${SKELETON_PATH}"
     else
         echo -e "${COLOR_START}Docker-skeleton${COLOR_STOP} already downloaded"
     fi
